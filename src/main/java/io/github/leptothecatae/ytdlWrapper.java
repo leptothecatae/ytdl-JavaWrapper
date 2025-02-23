a2ff6f6e863922d85b1a28b99f2601e9cc50d744
4ND1 <43019796+NECR0204@users.noreply.github.com>
2025-02-23 22:30:28 +0100

    reorganized format class; adapted for new executable

--- src/main/java/io/github/leptothecatae/ytdlWrapper.java
+++ src/main/java/io/github/leptothecatae/ytdlWrapper.java
@@ -4,34 +4,54 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 
 import io.github.leptothecatae.util.Exceptions.failedYTDLExecutionException;
-
+import io.github.leptothecatae.util.*;
 public class ytdlWrapper {
 
     private RegexHelper regexhelper = new RegexHelper();
     public ytdlWrapper() {
     }
-    public void getAvailableFormats(String url) throws failedYTDLExecutionException, IOException {
-        List<String> out;
-        ArrayList<String> in = new ArrayList<String>();
-        in.add("--list-formats");
-        in.add(url);
-        out = executeCommand(in);
-        for(int i = 0; i < out.size(); i++){
-            if(out.get(i).matches("^[0-9].*"))
+    public List<Format> getAvailableFormats(URL url) throws failedYTDLExecutionException, IOException {
+        List<String> stdout;
+        //List<VideoFormat> availableFormats = new ArrayList<VideoFormat>();
+        List<Format> availableFormats = new ArrayList<>();
+        ArrayList<String> commandArgs = new ArrayList<String>();
+        commandArgs.add("--list-formats");
+        commandArgs.add(url.toString());
+        stdout = executeCommand(commandArgs);
+        for(int i = 0; i < stdout.size(); i++){
+            if(regexhelper.startsWithNumber(stdout.get(i))){
                 //System.out.println(out.get(i));
-                regexhelper.parseVideoFormat(out.get(i));
+                //availableFormats.add(new VideoFormat(stdout.get(i), regexhelper));
+                availableFormats.add(new Format(stdout.get(i), regexhelper));
+            }
         }
         //out.forEach(System.out::println);
+        return availableFormats;
+    }
+
+    public void fetchVideo(URL url, String videoFormatCode, String audioFormatCode) throws failedYTDLExecutionException, IOException {
+        ArrayList<String> cmd = new ArrayList<String>();
+        List<String> stdout;
+        cmd.add("-f");
+        cmd.add(videoFormatCode + "+" + audioFormatCode);
+        cmd.add(url.toString());
+        stdout = executeCommand(cmd);
+
+        stdout.forEach(System.out::println);
+    }
+    public void fetchVideo(URL url, Format videoFormat, Format audioFormat) throws failedYTDLExecutionException, IOException {
+        fetchVideo(url, videoFormat.getFormatCode(), audioFormat.getFormatCode());
     }
     private List<String> executeCommand(List<String> params) throws IOException, failedYTDLExecutionException {
         // adding executable path to start of params so it can be directly parsed into processbuilder
         //TODO maybe include win and unix binaries and dont hardcode it here ifykwim
-        params.add(0, getClass().getResource("/youtube-dl.exe").getPath());
+        params.add(0, getClass().getResource("/yt-dlp.exe").getPath());
         ProcessBuilder pb = new ProcessBuilder(params);
         Process process = pb.start();
 
